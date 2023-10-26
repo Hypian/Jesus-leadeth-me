e331a00fcec8a9d6de5c4d61e1cee46033311317
Bryan <kagingobrian2002@gmail.com>
2023-10-26 20:56:11 +0300

    figuring out how to implement day printer

--- demoto.js
+++ demoto.js
@@ -169,3 +169,18 @@ default :
 answerr = "Not Aplicable";
 }
 console.log(`The result of Your Operation is  ${answerr}`)
+// creating Write a program that takes a number (1-7) as input and 
+// displays the corresponding day of the week (e.g., 1 for "Sunday," 2 for "Monday," etc.).
+
+let dayName = "";
+switch(dayNum){
+
+
+
+
+
+
+
+
+    
+}
