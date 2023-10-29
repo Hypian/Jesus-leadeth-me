8f392eac922efe1fd18d5024e0e407edea25e9df
Hypian <kagingobrian2002@gmail.com>
2023-10-29 22:51:03 +0200

    Stand in Line fcc

--- demoto.js
+++ demoto.js
@@ -276,3 +276,20 @@ const addiTion = (a,b,c) => {
 }
 console.log((addiTion(1,2,3)))
 greet();
+//Stand in line courtesy of freecode camp this code demonstates queues in js 
+function nextInLine(arr, item) {
+  // Only change code below this line
+  arr.push(item)
+  
+  return arr.shift();
+  
+  // Only change code above this line
+}
+
+// Setup
+let testArr = [1, 2, 3, 4, 5];
+
+// Display code
+console.log("Before: " + JSON.stringify(testArr));
+console.log(nextInLine(testArr, 6));
+console.log("After: " + JSON.stringify(testArr));
