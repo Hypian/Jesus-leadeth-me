c66f8f547f99bcc31745d0245d149e1a1cc57a04
Dev_Brian <124306840+Hypian@users.noreply.github.com>
2023-11-24 21:22:57 +0200

    Update demoto.js

--- demoto.js
+++ demoto.js
@@ -202,7 +202,7 @@ dayName = "Not Allowed";
 console.log(`The day of the week is ${dayName}`);
 //Simulate a traffic light with three states (red, yellow, green) and print the
 // color of the light when a number (1-3) is input.
-// we will need two variable colorLight and numero
+// we will need two variable colorLight and numero;
 var numero = 1;
 var resultant ;
 switch(numero){
