3d993fcb39377f5285ea30bd66f12b1f81808544
Dev_Brian <124306840+Hypian@users.noreply.github.com>
2023-12-04 17:39:39 +0200

    Update demoto.js

--- demoto.js
+++ demoto.js
@@ -284,7 +284,7 @@ function nextInLine(arr, item) {
   // Only change code below this line
   arr.push(item)
   
-  return arr.shift();
+  return arr.shift()
   
   // Only change code above this line
 }
