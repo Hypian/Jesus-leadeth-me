87c81721bfd6c492237002e3a0fdcab316913c04
Dev_Brian <124306840+Hypian@users.noreply.github.com>
2023-12-09 00:10:06 +0200

    Update demoto.js

--- demoto.js
+++ demoto.js
@@ -128,7 +128,7 @@ console.log(result)
 // regex but match keyword
 // as we saw before the test method or function returns booleans while the match;
 // now we use the match method
-// this one returns an array of matched strings or null if no matches are found
+// this one returns an array of matched strings or null if no matches are found;
 let wordss = "I hate Fox , i wish fox were dead, because fox at my friend lol"
 let myRegex = /fox/ig
 let answer = wordss.match(myRegex)
