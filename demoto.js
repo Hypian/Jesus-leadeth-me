1ba3134bc4afe72ea9e5dd4fb24a781737f946aa
Dev_Brian <124306840+Hypian@users.noreply.github.com>
2023-12-27 19:31:31 +0200

    Update demoto.js

--- demoto.js
+++ demoto.js
@@ -64,7 +64,7 @@ function countDownn(num){
 countDown(10);
 
 // arrays 
-const myArray = [1,2,3,4,5,6,7,8,9,10,11,12,14]
+const myArray = [1,2,3,4,5,6,7,8,9,10,11,12,14];
 var fillled = myArray.fill(0)
 console.log(fillled)
 let yas = [1,2,3,4,5,6,7,8,9,0]
